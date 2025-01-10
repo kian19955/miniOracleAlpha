e1d8a8cadcf6e35c8efdbcebbdfab78fa5eecaa7
kian19955 <kiansharifi84@gmail.com>
2025-01-10 18:15:27 +0330

    3

--- backtestMain.py
+++ backtestMain.py
@@ -16,7 +16,7 @@ def fetch(i):
     data = pd.read_csv("historical_data.csv")
     return data.iloc[i:400+i]
 
-while t<13421 :
+while t<8321 :
     print(i)
     for ticker in tickers:
 
@@ -26,7 +26,7 @@ while t<13421 :
             if portfolio["BTCUSDT"]["coin"] != 0:
                 l.append(portfolio["BTCUSDT"]["coin"] * data["Close"].iloc[-1])
             else: l.append(portfolio["BTCUSDT"]["balance"])
-        if t == 13420:
+        if t == 8320:
             if portfolio["BTCUSDT"]["coin"] != 0:
                 print(portfolio["BTCUSDT"]["coin"] * data["Close"].iloc[-1],n)
             else:print(portfolio["BTCUSDT"]["balance"],n)
