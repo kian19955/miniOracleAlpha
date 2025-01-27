2d2143a5e2a4dbd9e76e1c15c05d49755e79da61
navid_yaghmaei <navidyaghmaei@gmail.com>
2025-01-27 14:59:32 +0100

    finished ga but not tested yet

--- Optimization/GeneticAlgorithm/geneticAlgorithm.py
+++ Optimization/GeneticAlgorithm/geneticAlgorithm.py
@@ -1,59 +1,47 @@
-from typing import Callable, Type, Optional, get_args, get_origin, Union
+import logging
+from typing import Type, Optional, get_args, get_origin, Union
 from types import MappingProxyType
-import typing
 from inspect import signature
-import builtins
 import random
-from enum import Enum
-from dataclasses import dataclass
+from time import sleep
+from multiprocessing import Pool
+from enum import EnumMeta
 
-from deap import base, creator, algorithms, tools
+from deap import base, creator, tools
 
 from backtester import backtest
 from dataAnalysis import analyze
+from oracleMaths import randfloat
+from Optimization.GeneticAlgorithm.types import MateTypeProbabilities, MutateTypeProbabilities
+from Optimization.GeneticAlgorithm.operators import gauss_clamp
 
 
 # TODO: support for list
-# TODO: EnumClasses for TypeOperators
 # TODO: better var names?
 
-# TODO: MateTypeProbabilities
-@dataclass
-class MutateTypeProbabilities:
-    FLOAT: float = 0.1
-    INT = 0.1
-    BOOL = 0.1
-    ENUM = 0.1
-    UNION_CHANGE = 0.1
+# TODO: move all operations to independent operator functions
 
-
-# These will be used to create a new individual or mutate one,
-# For enums you are allowed to set a probability for each enum value, Enum values not given dont have a chance to mutate into
-# TODO: use genome Settings
-#1. First look for EnumClasses for TypeOperators
-{
-    "param1": {
-        float: {"start": float, "stop": float, "step": float},
-        MutateTypeProbabilities: {MutateTypeProbabilities.ENUM: 0.1, MutateTypeProbabilities.UNION_CHANGE: 0.1}
-    }
-}
-
-
-class geneticAlgorithm:
+class GeneticAlgorithm:
     def __init__(
             self,
             tc: Type,
             bt_settings: dict[str, any],
-            create_individual: Callable,
             key_genomes: dict[str, any],
+
             genome_settings: dict[str, dict[type, dict[str | type, any]]] = None,
+
             blacklist_genes: Optional[tuple[str]] = None,
             whitelist_genes: Optional[tuple[str]] = None,
-            base_params: Optional[Type] = None,
+            base_params: Optional[dict[str, any]] = None,
+
             float_blend: Optional[float] = 0.5,
             int_blend: bool = False,
-            genome_mate_percentage: float = 0.5,
-            mutate_tp: MutateTypeProbabilities = MutateTypeProbabilities
+
+            mate_tp: MateTypeProbabilities = MateTypeProbabilities,
+            mutate_tp: MutateTypeProbabilities = MutateTypeProbabilities,
+            mutation_strength: float = 0.1,
+
+            tournament_size: int = 2
     ):
         """
 
@@ -61,7 +49,6 @@ class geneticAlgorithm:
         :param bt_settings: Key is parameter name, value is weight where
         1 tries to get the value as high as possible and
         -1 tries to get the value as low as possible.
-        :param create_individual:
         :param key_genomes:
         :param genome_settings: These will be used to create a new individual or mutate one. For enums you are allowed to set a probability for each enum value, Enum values not given dont have a chance to mutate into
         genome_settings: dict[str, dict[type, dict[str | type, any]]] = {
@@ -77,19 +64,24 @@ class geneticAlgorithm:
         if blacklist_genes is not None and whitelist_genes is not None:
             raise ValueError("blacklist_genes and whitelist_genes cannot be used together.")
 
+        self.validate_genome_settings(genome_settings)
+
         self.tc: Type = tc
         self.bt_settings: dict = bt_settings
-        self.create_individual: Callable = create_individual
         self.key_genomes: dict = key_genomes
         self.genome_settings: dict[str, dict[type, dict[str | type, any]]] = genome_settings
 
         self.float_blend = float_blend
         self.int_blend = int_blend
-        self.genome_mate_percentage = genome_mate_percentage
+
+        self.mate_tp = mate_tp
         self.mutate_tp = mutate_tp
+
+        self.mutation_strength = mutation_strength
 
         self.base_params = base_params
         self.tc_params = signature(tc.__init__).parameters
+
         mutable_params = dict(self.tc_params)
 
         for param_name in self.tc_params.keys():
@@ -104,11 +96,91 @@ class geneticAlgorithm:
         creator.create("Individual", dict, fitness=creator.FitnessMax)
 
         toolbox = base.Toolbox()
-        toolbox.register("individual", tools.initIterate, creator.Individual, create_individual)
+        toolbox.register("individual", tools.initIterate, creator.Individual, self.create_individual)
         toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
         toolbox.register("evaluate", self.evaluate)
+        toolbox.register("mate", self.mate)
         toolbox.register("mutate", self.mutate)
+        toolbox.register("select", tools.selTournament, tournsize=tournament_size)
+
+        self.toolbox = toolbox
+
+
+    def validate_genome_settings(self, genome_settings):
+        for param_name, param in self.tc_params.items():
+            annotation = param.annotation
+
+            if annotation is int or annotation is float:
+                if not hasattr(self.genome_settings[param_name][annotation], 'start') or not hasattr(
+                        self.genome_settings[param_name][annotation], 'stop'):
+                    raise ValueError(
+                        f"Genome Settings for {param_name} must have a start and a stop value for {annotation}"
+                    )
+
+    def run(self, generations: int = 40, population: int = 50):
+        pool = Pool()
+        self.toolbox.register("map", pool.map)
+
+        population: list = self.toolbox.population(n=population)
+
+        fitnesses = self.toolbox.map(self.toolbox.evaluate, population)
+        for ind, fit in zip(population, fitnesses):
+            ind.fitness.values = fit
+
+        for gen in range(generations):
+            survivers = self.toolbox.select(population, len(population))
+            survivers = list(map(self.toolbox.clone, survivers))
+
+            # Mating
+            for child1, child2 in zip(survivers[::2], survivers[1::2]):
+                self.toolbox.mate(child1, child2)
+
+            for child in survivers:
+                self.toolbox.mutate(child)
+
+            # Evaluate the individuals with an invalid fitness
+            fitnesses = self.toolbox.map(self.toolbox.evaluate, survivers)
+            for ind, fit in zip(survivers, fitnesses):
+                ind.fitness.values = fit
+
+            population[:] = survivers
+
+            print(f"Generation {gen + 1}: Best Sharpe = {tools.selBest(population, k=1)[0].fitness.values[0]}")
+
+        pool.close()
+        pool.join()
+
+        return population
+
+    def create_individual(self):
+        genome: dict[str, any] = {}
+        for param_name, param in self.tc_params.items():
+            possible_types: list[type] = list(self.genome_settings[param_name].keys())
+            chosen_type: type = random.choice(possible_types)
+
+            if chosen_type is float:
+                genome[param_name] = randfloat(
+                    self.genome_settings[param_name][float]['start'],
+                    self.genome_settings[param_name][float]['stop'],
+                    self.genome_settings[param_name][float].get('step', None)
+                )
+
+            if chosen_type is int:
+                genome[param_name] = random.randrange(
+                    self.genome_settings[param_name][int]['start'],
+                    self.genome_settings[param_name][int]['stop'],
+                    self.genome_settings[param_name][int].get('step', 1)
+                )
+
+            if isinstance(chosen_type, EnumMeta):
+                genome[param_name] = self._retrieve_random_enum(param_name, chosen_type)
+
+            if chosen_type is type(None):
+                genome[param_name] = None
+
+            else:
+                raise ValueError(f"Unsupported type {chosen_type}")
 
     def evaluate(self, individual):
         individual = self.tc(**(individual | self.base_params))
@@ -118,8 +190,8 @@ class geneticAlgorithm:
             ticker=self.bt_settings['ticker'],
             days=self.bt_settings['days'],
             interval=self.bt_settings['interval'],
-            trade_long=getattr(self.bt_settings, 'trade_long', True),
-            trade_short=getattr(self.bt_settings, 'trade_short', True),
+            trade_long=self.bt_settings.get('trade_long', True),
+            trade_short=self.bt_settings.get('trade_short', True),
             use_csv=True
         )
 
@@ -127,8 +199,8 @@ class geneticAlgorithm:
             target_filename=self.bt_settings['ticker'] + "_" + str(self.bt_settings['days']) + "_" + self.bt_settings[
                 'interval'] + ".csv",
             bt_df=bt_df,
-            trade_long=getattr(self.bt_settings, 'trade_long', True),
-            trade_short=getattr(self.bt_settings, 'trade_short', True),
+            trade_long=self.bt_settings.get('trade_long', True),
+            trade_short=self.bt_settings.get('trade_short', True),
         )
 
         values: tuple = tuple(performance[attr] for attr in self.key_genomes.keys())
@@ -137,8 +209,7 @@ class geneticAlgorithm:
 
     def mate(self, child1, child2):
         for param_name, param in self.tc_params.items():
-            if random.random() > self.genome_mate_percentage:
-                continue
+            roll: float = random.random()
 
             annotation = param.annotation
             val1 = child1[param_name]
@@ -150,37 +221,79 @@ class geneticAlgorithm:
                 annotation = type(val1)
 
             if self.float_blend is not None and annotation is float:
+                if roll > self.mate_tp.FLOAT:
+                    continue
+
                 tools.cxBlend(val1, val2, alpha=self.float_blend)
 
             if self.int_blend and annotation is int:
+                if roll > self.mate_tp.INT:
+                    continue
+
                 blended_int: int = (val1 + val2) // 2
                 child1[param_name], child2[param_name] = blended_int, blended_int
 
             # Unified handling for all other types (int, Enum, Union, etc.)
             # And Fallback for unsupported types
             else:
+                if isinstance(annotation, EnumMeta):
+                    if roll > self.mate_tp.ENUM:
+                        continue
+
+                elif get_origin(annotation) is Union:
+                    if roll > self.mate_tp.UNION:
+                        continue
+
+                elif annotation is bool:
+                    if roll > self.mate_tp.BOOL:
+                        continue
+
+                else:
+                    logging.warning(f"Unsupported type detected when mating for {param_name}: {annotation}")
+                    if roll > self.mate_tp.OTHER:
+                        sleep(1)
+                        continue
+
                 child1[param_name], child2[param_name] = val2, val1
 
-    def mutate(self, individual):
-        for param_name, param in self.tc_params.items():
-            roll: float = random.random()
+    def mutate(self, individual, custom_params_settings: Optional[dict[str, any]] = None):
+        param_settings: dict[str, any] = custom_params_settings if custom_params_settings is not None \
+            else self.tc_params
 
+        for param_name, param in param_settings.items():
+            roll: float = random.random()
             annotation = param.annotation
-            val = individual[param_name]
+            val: any = individual[param_name]
+
+            def roll_check(mutate_prob, mutation_func) -> bool:
+                if roll < mutate_prob:
+                    individual[param_name] = mutation_func()
+                    return True
+                return False
 
             if annotation is float:
-                if roll > self.mutate_tp.FLOAT:
+                if roll_check(self.mutate_tp.FLOAT, lambda:
+                gauss_clamp(
+                    val,
+                    self.genome_settings[param_name][float]['start'],
+                    self.genome_settings[param_name][float]['stop'],
+                    self.genome_settings[param_name][float].get('step', None),
+                    False,
+                    self.mutation_strength
+                )):
                     continue
-                tools.mutGaussian(individual, mu=0, sigma=0.1, indpb=0.1)
 
             elif annotation is int:
-                if roll > self.mutate_tp.INT:
-                    continue
-                individual[param_name] = random.randrange(
+                if roll_check(self.mutate_tp.INT, lambda:
+                gauss_clamp(
+                    val,
                     self.genome_settings[param_name][int]['start'],
                     self.genome_settings[param_name][int]['stop'],
-                    self.genome_settings[param_name][int]['step']
-                )
+                    self.genome_settings[param_name][int].get('step', None),
+                    True,
+                    self.mutation_strength
+                )):
+                    continue
 
             elif annotation is bool:
                 if roll > self.mutate_tp.BOOL:
@@ -188,66 +301,62 @@ class geneticAlgorithm:
                 individual[param_name] = not val
 
             elif isinstance(annotation, EnumMeta):
-                if roll > self.mutate_tp.ENUM:
+                if roll_check(self.mutate_tp.ENUM, lambda:
+                    self._retrieve_random_enum(param_name, annotation)
+                ):
                     continue
-                individual[param_name] = random.choice(list(annotation))
 
-                # Handle Optional (Union with None)
+            # Handle Optional (Union with None)
             elif get_origin(annotation) is Union:
-                args = get_args(annotation)
-                if type(None) in args:
-                    if roll <= self.mutate_tp.UNION_NONE:
-                        individual[param_name] = None
+                if roll_check(self.mutate_tp.UNION, lambda:
+                    self._mutate_union(param_name, val, annotation)
+                ):
+                    continue
 
-                elif individual[param_name] is None:
-                    # If currently None, assign a random valid value
-                    non_none_type = [t for t in get_args(annotation) if t is not type(None)][0]
-                    if non_none_type is float:
-                        individual[param_name] = random.uniform(0, 1)
-                    elif non_none_type is int:
-                        individual[param_name] = random.randint(0, 100)
-                    elif isinstance(non_none_type, EnumMeta):
-                        individual[param_name] = random.choice(list(non_none_type))
-
-                # Fallback for unsupported types
             else:
-                if random.random() < 0.1:  # 10% chance to randomize
-                    individual[param_name] = random.choice(list(annotation))
+                logging.warning(f"Unsupported type detected for {param_name}: {annotation}")
+                sleep(1)
+
+    def _mutate_union(self, param_name: str, val: any, annotation: tuple[any, ...]):
+        if random.random() > self.mutate_tp.UNION_NEW_TYPE:
+            arg = random.choice(get_args(annotation))
+        else:
+            return self.mutate(
+                {param_name: val},
+                {self.tc_params[param_name].name: self.tc_params[param_name]}
+            )
+
+        if arg is type(None):
+            return None
+
+        elif arg is float:
+            return randfloat(
+                self.genome_settings[param_name][float]['start'],
+                self.genome_settings[param_name][float]['stop'],
+                self.genome_settings[param_name][float].get('step', None),
+            )
+        elif arg is int:
+            return random.randrange(
+                self.genome_settings[param_name][int]['start'],
+                self.genome_settings[param_name][int]['stop'],
+                self.genome_settings[param_name][int].get('step', 1),
+            )
+        elif isinstance(arg, EnumMeta):
+            return self._retrieve_random_enum(param_name, arg)
+
+    def _retrieve_random_enum(self, param_name, enum_class):
+        try:
+            enum_settings: dict[type, any] = self.genome_settings[param_name][enum_class]
+            roll = random.uniform(0, sum(enum_settings.values()))
+
+            for enum_value, prob in enum_settings.items():
+                roll -= prob
+                if roll <= 0:
+                    return enum_value
+
+        except KeyError:
+            return random.choice(list(enum_class))
 
 
 if __name__ == '__main__':
-    from enum import Enum, EnumType, EnumMeta, IntEnum, auto
-
-
-    class MyEnum(IntEnum):
-        A = 1
-        B = 2
-
-
-    class Test:
-        def __init__(self, flo: float, intiger: int, option: Optional[str], enum: MyEnum):
-            self.flo = flo
-            self.intiger = intiger
-            self.option = option
-            self.enum = enum
-
-
-    tc = Test
-    tc_params = signature(tc).parameters
-    print(tc_params)
-    for param_name, param in tc_params.items():
-        annotation: Type = param.annotation
-
-        match annotation:
-            case builtins.float:
-                print(f"{param_name}: Float")
-            case builtins.int:
-                print(f"{param_name}: Integer")
-            case _ if isinstance(annotation, EnumMeta):
-                print(f"{param_name}: Enum")
-            case _ if hasattr(annotation, '__origin__') and annotation.__origin__ is typing.Union:
-                if len(annotation.__args__) == 2 and type(None) in annotation.__args__:
-                    print(f"{param_name}: Optional")
-                else:
-                    print(f"{param_name}: Union")
-                print(type(None))
+    from tradingComponents.indicators import relativeStrengthIndex
