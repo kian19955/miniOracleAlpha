5b3c38d0b511b4f4a28eb630213349a2655be991
MrCode200 <navidyaghmaei@gmail.com>
2025-02-10 19:16:52 +0100

    fix(constants): create directories if not exist
    Ensure directories defined in constants.py exist.
    
    - Fixes a bug where the application would fail if the necessary data directories

--- constants.py
+++ constants.py
@@ -6,7 +6,7 @@ bt_data_dir_path = Path(__file__).parent / 'data' / 'backtestData'
 ga_his_dir_path = Path(__file__).parent / 'data' / 'gaHistoryData'
 
 for dir_path in [market_his_dir_path, bt_data_dir_path, ga_his_dir_path]:
-    if os.path.exists(dir_path):
+    if not os.path.exists(dir_path):
         os.makedirs(dir_path)
         print(f"Folder {dir_path} not found, creating Folder.")
 
